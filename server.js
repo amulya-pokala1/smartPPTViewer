2626f6e81c7105fcc80a32080db90bc95a1e93ca
amulya-pokala1 <amulya.pokala@accoliteindia.com>
2018-01-14 23:05:33 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/assets/bootstrap', express.static(__dirname + '/node_modules/bootstrap
 app.use('/assets/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
 
 var users = [];
-
+var chat="";
 io.on("connection", function(socket){
 
   socket.emit('welcome','Socket #'+ socket.id);
@@ -20,16 +20,27 @@ io.on("connection", function(socket){
   {
     users.push({id : socket.id, name : user.name});   
     socket.join(socket.id); 
+    socket.emit("user_onboard",{"id":socket.id, "msg": chat});
     io.sockets.emit("refresh_users", users);   //  socket.broadcast.emit will emit all clients except this connected socket
   });
-
+  socket.on("global_chat",function(obj){
+    console.log(obj);
+    chat=obj.msg;
+    io.sockets.emit("global_chat", obj); 
+  })
   socket.on("slide_update", function (slideIndex) {
-    socket.broadcast.emit("slide_update", slideIndex);
+    io.sockets.emit("slide_update", slideIndex);
   });
 
+  socket.on("chat_ppt", function(obj){
+    io.sockets.emit("chat_ppt",obj);
+  })
   socket.on("chat_msg", function(chatObj){
-    
-    socket.in(chatObj.to).emit( { from : users.filter(x=>x.id=== socket.id),  msg :  chatObj.msg}); 
+   
+    var user=  users.filter(x=>x.id === socket.id);
+    console.log(user[0].name);
+    socket.in(chatObj.to).emit("chat_msg",{ from : user[0].name,  msg :  chatObj.msg, fid: chatObj.from}); 
+
   });
 
   socket.on("disconnect", function(data)
@@ -39,6 +50,7 @@ io.on("connection", function(socket){
   });  
 
 });
+
 
 
 
